f52b4d4fe43e435db1ecf952eec3b114ad5b6e90
NeonCarbide <deadstream37@gmail.com>
2021-02-01 19:03:47 +0000

    Added toast notifications

--- timer/app.js
+++ timer/app.js
@@ -178,10 +178,12 @@ function notify(index) {
     delay = 3000;
     h = alt1.rsHeight;
     w = alt1.rsWidth;
-    // x = 20;
-    x = h - size - (size + 20);
+    x = 20;
+    // x = h - size - (size + 20);
     y = parseInt(w / 2 - (text.length * size) / 3.15);
     colour = parseInt('0xFF' + getColourFromString('#C0C030'));
+
+    console.log(h, w);
 
     alt1.overLayText(text, colour, size, x, y, delay);
   } else {
