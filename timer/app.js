ca68a986ebf1f073ff059910259c08fe4f5f4ee9
NeonCarbide <deadstream37@gmail.com>
2021-02-03 19:44:36 +0000

    Small changes to overlay

--- timer/app.js
+++ timer/app.js
@@ -229,9 +229,6 @@ function overlayNotify() {
     // x = h - size * 2 + 20;
     // y = size;
     y = parseInt(h / 2 - size / 2)
-
-    console.log(w, y);
-
     // y = parseInt(w / 2 - (text.length * size) / 2.15);
     colour = parseInt('0xFFF0F000');
 
