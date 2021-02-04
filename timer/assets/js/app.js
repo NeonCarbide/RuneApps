4cf466706257b715a3fd6713edefe896ddcf1861
NeonCarbide <deadstream37@gmail.com>
2021-02-04 06:14:00 +0000

    GUI Updates

--- timer/assets/js/app.js
+++ timer/assets/js/app.js
@@ -317,9 +317,9 @@ function getTextColourRelativeToBG(colour) {
       .join('');
   }
 
-  r = parseInt(hex.substring(0, 2), 16);
-  g = parseInt(hex.substring(2, 2), 16);
-  b = parseInt(hex.substring(4, 2), 16);
+  r = parseInt(hex.substr(0, 2), 16);
+  g = parseInt(hex.substr(2, 2), 16);
+  b = parseInt(hex.substr(4, 2), 16);
 
   yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
